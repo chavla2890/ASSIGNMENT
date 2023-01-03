c22794297418fc8eeb6f760149f19c3d78eedd8b
chavla2890 <akashchawla497@gmail.com>
2023-01-03 20:45:27 +0530

    javaScript

---
+++ tops/JavaScript/hello.js
@@ -0,0 +1,164 @@
+        //1. ways to print in javaScript
+        //console.log("hello world")
+        //alert("me")
+        // document.write("this is documenet write");  
+        
+        //2. JavaScript console API
+        // console.log("hello world "+ (25+25)+" total")
+        // console.warn("this is warning")      
+        // console.error("this is an error")
+        // console.clear
+
+        // 3. Variable :- it is a container which store the value 
+        // var number1 = 34;
+        // var number2 = 36;
+        // console.log(number1+number2);
+
+        //  4. perimitive dataTypes in javaScript are :- number, string, boolean, undefines, null, symbol
+        // var str1 ="this is a string";
+        // var str2 ='this is also a string';
+        // integer or number
+        // var number1 = 34;
+       
+
+        
+        // boolean
+        // var a=true;
+        // var b=false;
+
+
+        // printin the dataType
+        // console.log(number1);
+        // console.log(number2);
+
+        // console.log(str1);
+        // console.log(str2);
+
+        
+
+        // console.log(a);
+        // console.log(b);
+
+
+        // undefined and null
+
+        // var und= undefined;
+        // console.log(und);
+
+        // var n= null;
+        // console.log(n);
+
+
+        // 5. refferance datatype in javascript arew :- object and arrays
+        // objects
+        // var marks = {
+        //     ravi : 34,
+        //     shubham : 36,
+        //     harrry : 35
+        // }
+        // console.log(marks);
+
+
+        // Array
+        // var arr=[1,"akash",2,3,4,5,6]
+        // console.log(arr);
+
+
+        // 5. operators in javaScript
+            // a. arthmetic operators are :- +, -, *, /, %
+            // b. assingment operator are :- =, +=, *=, -=, /=, %=
+            // c. logical operator are :- !(not), ||(or), &&(and)
+            // d. comparesion opertors are :- ==(equal to), <(less than), >(greater  than), <=(less than  equals to), >=(greater than equals tu) 
+
+        // 6. Functions in java script
+        
+        // function av(a, b){
+        //     c=(a+b)/2;
+        //     return c;
+        // }
+
+        // c1 = av(4, 6);
+        // c2 = av(14, 16);
+
+        // console.log(c1, c2);
+
+
+        // 7. condition statement in javaScript  
+        // var age= 3;
+        // if(age>=18){
+        //     console.log("you can vote");
+        // }else{
+        //     console.log("jaa ghar pe so jaa");
+        // }
+
+        // 8. loops in javascript
+            // var arr=[1,'aaksh',5,6,45,25,26];
+        // a. for loop
+        // for(var i=0; i<=4;i++){
+        //     console.log(arr[i]);
+        // } 
+         
+        // b. for each loop
+        // arr.forEach(function(element){
+        //     console.log(element);
+        // })
+
+        // c. while loop
+        // let i = 0;
+        // while(i<arr.length){
+        //     console.log(arr[i]);
+        //     i++;
+        // }
+
+        // d. do while loop
+        // let i=0;
+        // do{
+        //     console.log(arr[i]);
+        //     i++
+        // }while(i<arr.length);
+
+
+        // 9. break and continue statement 
+        // var arr=[1,'aaksh',5,6,45,25,26];
+
+        // a. break
+            // for(var i=0; i<=arr.length;i++){
+            //     console.log(arr[i]);
+            //     if(i==5){
+            //         break;
+            //   }
+            //  } 
+
+        // b. continue
+        
+        // for(var i=0; i<=arr.length;i++){
+        //     console.log(arr[i]);
+        //     if(i==5){
+        //         continue;
+        //     }
+        // }
+
+        // 10. array methods 
+        // let myArray=["fan","camera",15,34,null,true];
+        // console.log(myArray.length); // length of array
+        // myArray.pop(); // usw to remove element from array from last
+        // myArray.push("aakash"); //use to add element in array
+        // myArray.shift(); // usw to remove element from array from first
+        // myArray.unshift("aakash"); // usw to shift element from array at last
+        // console.log(myArray);
+
+        // 11. string method in javascript
+
+        // let str="hello world i am learning java script";
+        // console.log(str.length);
+        // console.log(str.indexOf("learning"));
+        // console.log(str.slice(0,5));
+        // console.log(str.replace("i am","aakash is"));
+
+
+        // let myDate =new Date();// to get current date and time 
+        // console.log(myDate);
+
+        // 12. dom manupulation
+
+        
